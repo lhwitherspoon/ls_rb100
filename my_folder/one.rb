12a16b80f9b2cbcf2e77a6eda25bbb50930c2bab
Les Witherspoon <les@lhwitherspoon.com>
2020-07-20 18:49:01 -0700

    First ruby files.

---
+++ my_folder/one.rb
@@ -0,0 +1,3 @@
+puts "This is file one"
+
+
---
+++ preparation.rb
@@ -0,0 +1,8 @@
+# Example ruby file
+# This line lets the file use `pry` for debugging. 
+require "pry"
+
+# some code
+# some more code 
+binding.pry # Code execution pauses at this point, opens new `pry` file to allow debugging.
+
