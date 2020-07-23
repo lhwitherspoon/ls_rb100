ab0d3bd344c3a70a6fc3805b0a3a63bc2d1f804e
Les Witherspoon <les@lhwitherspoon.com>
2020-07-23 16:17:46 -0700

    Completed program to square three floats of my own choosing.

---
+++ basics/float_three.rb
@@ -0,0 +1,24 @@
+# Write a program to calculate the squares of three float numbers of my choosing. 
+# I assume here that it is to take three separate numbers and produce the result in one operation.
+
+
+puts "What three numbers do you want to square? Please list them this way: '1, 2, 3'."
+num_string = gets.chomp
+my_floats = num_string.split(", ")
+
+
+make_float = Proc.new {| item | item.to_f}
+square_float = Proc.new { 
+  |item |
+  item ** 2}
+
+my_floats.map!(&make_float)
+my_floats.map!(&square_float)
+puts my_floats
+
+# Maybe I'm overengineering this. Since the answer was along the lines of "puts 6.13 * 6.13"
+
+puts "What does the following error message tell me? \n SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'"
+
+puts "On line 2 of an irb session, a parentheses was typed instead of a curly brace."
