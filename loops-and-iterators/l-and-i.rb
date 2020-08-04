cb21338f0f61f29a4d36db3cce48682f197d10dd
Les <les.witherspoon@gmail.com>
2020-08-03 17:08:00 -0700

    Edited file to make answer clearer. Changes to be committed: modified:   l-and-i.rb

--- loops-and-iterators/l-and-i.rb
+++ loops-and-iterators/l-and-i.rb
@@ -5,10 +5,10 @@ x.each do |a|
   a + 1
 end
 puts x
-# This returns each element of x (represented by a) + 1
+# Initial answer: This returns each element of x (represented by a) + 1
 # 2
 # 3
 # 4
 # 5
 # 6
-# wrong - it returns the array itself, as the .each method returns the array that it is called up. 
+# ETA: Initial answer is wrong - it returns the array itself, as the .each method returns the array that it is called up. 
