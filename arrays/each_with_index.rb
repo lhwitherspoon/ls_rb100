1f18c72a464135ca8636778749ef4fac2b9e0dd6
Les Witherspoon <les.witherspoon@gmail.com>
2020-08-09 23:26:01 +0000

    Changes to be committed: 	new file:   each_with_index.rb 	new file:   error_msg.rb 	new file:   method_return.rb 	new file:   plus_two.rb 	new file:   return_example.rb 	new file:   what_value.rb

---
+++ arrays/each_with_index.rb
@@ -0,0 +1,3 @@
+pizza = ['dough', 'cheese', 'pizza sauce', 'anchovies']
+
+pizza.each_with_index { |ingredient, index| print "#{index} #{ingredient}, " }
---
+++ arrays/what_value.rb
@@ -0,0 +1,7 @@
+# What is the value of a, b, and c in the following program? 
+
+string = "Welcome to America!"
+a = string[6] # value is `e`
+b = string[11] # value is A
+
+c = string[19] #Does not return a value - out of bounds for the index. 
